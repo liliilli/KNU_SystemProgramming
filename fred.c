2364dc159d6737036e54d391c328312ef7392150
Ktaewon <dydalehdxo@naver.com>
2018-12-24 11:35:14 +0900

    Update

---
+++ fred.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void fred(int arg)
+{
+	printf("Fred : You passed %d\n", arg);
+}
---
+++ helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	for (int i = 0; i < 50; ++i)
+	{
+		printf("[%d] Hello world!\n", i);
+	}
+	return 0;
+}
+
