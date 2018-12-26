037e778df4fa2e5bed481179d797f9106e2ed529
Ktaewon <dydalehdxo@naver.com>
2018-12-26 09:03:03 +0900

    Move files to sub-directory

---
+++ _181224/bill.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void bill(const char* arg)
+{
+	printf("Bill : you passed %s\n", arg);
+}
---
+++ _181224/hello2/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void func(void);
+
+int main(int argc, char** argv)
+{
+	printf("Hello world!\n");
+	func();
+	return 0;
+}
