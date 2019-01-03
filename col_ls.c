3a071ba6c6472b940f4183302deff3d0938d6a0f
liliilli <liliilli@gmail.com>
2019-01-03 14:16:38 +0900

    2018-01-03

--- col_ls.c
+++ col_ls.c
@@ -3,6 +3,8 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <sys/ioctl.h>
 #include <sys/types.h>
@@ -12,6 +14,11 @@
  * @brief Process files and get attribute, print them.
  */
 void ProcessLs(const char* iDirName);
+
+/*
+ * @brief Get size of screen dimension.
+ */
+void GetPrintScreenSize(unsigned int* row, unsigned int* col);
 
 int main(int argc, char** argv)
 {
@@ -28,26 +35,101 @@ int main(int argc, char** argv)
 	}
 }
 
+
 void ProcessLs(const char* iDirName)
 {
+	/* Check dir is exist */
 	DIR* ptrDir = NULL;
-	struct dirent* ptrDirent = NULL;
-	int closeFlag = 0;
-
 	if ((ptrDir = opendir(iDirName)) == NULL)
 	{
 		fprintf(stderr, "ls1 : Failed to open directory %s\n", iDirName);
 		return;
 	}
-
-	while ((ptrDirent = readdir(ptrDir)) != NULL)
-	{
-		printf("%s\n", ptrDirent->d_name);
-	}
 	
-	closeFlag = closedir(ptrDir);
+	/* Get window consle row & column size */ 
+	unsigned winCol, winRow;
+	GetPrintScreenSize(&winRow, &winCol);
+
+	/* Get `alphabet sorted` entry list of `iDirName` directory. */
+	struct dirent** entryList = NULL;
+	int entryCount = scandir(iDirName, &entryList, NULL, alphasort);
+	if (entryCount < 0) { perror("scandir"); }
+	else
+	{
+		int i = 0;
+		while (i < entryCount) { printf("%s\n", entryList[i++]->d_name); }
+	}
+
+	/* Calculate column item length. */
+	int maxColItem = 1;
+	int* colMaxLenList = (int*)malloc(entryCount * sizeof(int));
+	int outFlag = 0;
+	do
+	{
+		memset(colMaxLenList, 0, sizeof(int) * entryCount);
+		/* Get max size of given maxColItem */
+		for (int i = 0; i < maxColItem; ++i)
+		{	
+			for (int j = i; j < entryCount; j += maxColItem)
+			{
+				int len = strlen(entryList[j]->d_name) + 3;
+				if (colMaxLenList[i] < len) { colMaxLenList[i] = len; }
+			}
+		}
+
+		unsigned sumLength = 0;
+		for (int i = 0; i < entryCount; ++i) { sumLength += colMaxLenList[i]; }
+
+		if (outFlag == 1) { break; }
+		if (sumLength > winCol) 
+		{ 
+			if (maxColItem == 1) { break; }
+			maxColItem -= 1;
+			outFlag = 1; 
+		}
+
+		if (maxColItem == entryCount) { break; } else { maxColItem += 1; }
+	}
+	while (1);
+	
+	/* Print Sequence */
+	for (int i = 0; i < entryCount; ++i)
+	{
+		/* Make meta string format of each column. */
+		char strFormat[16] = {'\0', };
+		strcat(strFormat, "%-");
+		char intStr[16] = {'\0', };
+		sprintf(intStr, "%d", colMaxLenList[i % maxColItem] - 3);
+		strcat(strFormat, intStr);
+		strcat(strFormat, "s ");
+
+		printf(strFormat, entryList[i]->d_name);
+		if ((i + 1) % maxColItem == 0) { printf("\n"); }
+	}
+
+	/* Free entryList */
+	int i = 0; 
+	while (i < entryCount) { free(entryList[i++]); }
+	free(entryList);
+	free(colMaxLenList);
+
+	int closeFlag = closedir(ptrDir);
 	if (closeFlag == -1)
 	{
 		fprintf(stderr, "ls1 : Failed to close directory sturcture instance %s\n", iDirName);
 	}
+}
+
+void GetPrintScreenSize(unsigned int* row, unsigned int* col)
+{
+	struct winsize sizeBuffer;
+	if (ioctl(0, TIOCGWINSZ, &sizeBuffer) == -1)
+	{
+		*row = 0;
+		*col = 0;
+		return;
+	}
+	
+	*row = sizeBuffer.ws_row;
+	*col = sizeBuffer.ws_col;
 }
